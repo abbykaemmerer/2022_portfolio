e23ec9ac9462f3975eedb48dda4b7c5c8877ac27
Abby Kaemmerer <abby.kaemmerer@twisthink.com>
2022-09-12 15:35:57 -0500

    Add TDH to port

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [

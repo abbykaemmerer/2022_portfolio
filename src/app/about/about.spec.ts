aad624bdc9803c6a7064a3fc858eb5833dba0e39
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2026-09-21 13:20:42 -0500

    home page updates

--- src/app/about/about.spec.ts
+++ src/app/about/about.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { About } from './about';
 
 describe('About', () => {
@@ -8,6 +9,7 @@ describe('About', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [About],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(About);
--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,25 +1,21 @@
-import { NgIf } from '@angular/common';
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [NgIf],
+  imports: [RouterLink],
   selector: 'app-about',
   styleUrl: './about.css',
   templateUrl: './about.html',
 })
-export class About implements OnInit {
-  public mobile = false;
+export class About {
+  /** Swap this path to change the homepage portrait. */
+  readonly portraitSrc = '/assets/img/headshots/trio.jpeg';
+  readonly portraitAlt =
+    'Three cafe portraits of Abby: looking up from a laptop, smiling at the camera, and working at a table.';
 
-  ngOnInit(): void {
-    this.updateMobile();
-  }
-
-  @HostListener('window:resize')
-  onResize(): void {
-    this.updateMobile();
-  }
-
-  private updateMobile(): void {
-    this.mobile = window.innerWidth < 630;
-  }
+  readonly funFacts = [
+    'May stop mid-conversation to identify a bug.',
+    'Passionate about sunsets and sour candy',
+    'Has an unreasonable amount of hometown pride for St. Louis.',
+  ];
 }
